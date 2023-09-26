63bde67d8a609950ad596798099231093e1f4483
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-26 04:39:45 -0500

    22. Processing & apply_force().

--- player.gd
+++ player.gd
@@ -3,4 +3,11 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_impulse(Vector2(25, 0))
+	# apply_impulse(Vector2(25, 0))
+	pass
+
+func _process(delta):
+	pass
+
+func _physics_process(delta):
+	apply_force(Vector2(25, 0))
