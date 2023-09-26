cce6249c8678ac24770b315a84a6208d7c8aa629
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-26 05:52:41 -0500

    27. Function Return Values

--- player.gd
+++ player.gd
@@ -2,6 +2,10 @@ extends RigidBody2D
 
 var force = 1500
 
+func _ready():	
+	var addition_result = add(4, 5)
+	print(addition_result)
+
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		apply_force(Vector2(force, 0))
@@ -11,3 +15,10 @@ func _physics_process(delta):
 		apply_force(Vector2(0, -force))
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2(0, force))
+
+func test():
+	var t = 10
+	return (t + 10)
+	
+func add(num1, num2):
+	return num1 + num2
