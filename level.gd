639d4129b485c55e626446a464f9f3c5641e485c
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-25 21:06:23 -0500

    18. Number Operations & More Printing

--- level.gd
+++ level.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Hello, World!!!")
+	pass
--- player.gd
+++ player.gd
@@ -3,9 +3,11 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	# test()
 	pass
+
+func test():
+	# print("This is the Test function " + "from the player script")
+	# + - * / **
+	print((2 + 3) * 5)
+	print("2 + 2 is " + str(4))
