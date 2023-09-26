0cf92c6fc7303599b783347624859753262fc98a
manuyeldev <manuyeldev@yelsfstudio.com>
2023-09-25 21:31:27 -0500

    20. Function Parameters / Arguments

--- player.gd
+++ player.gd
@@ -3,15 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	test()
+	printDoubled(10)
 
-func test():
-	# print("This is the Test function " + "from the player script")
-	# + - * / **
-	var my_variable = "Hello"
-	print(my_variable)
-	print(my_variable)
-	print(my_variable)
-	my_variable = "Good Bye"
-	print(my_variable)
-	print(my_variable)
+func printDoubled(input_number):
+	print(input_number * 2)
